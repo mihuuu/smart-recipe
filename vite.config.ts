e1b2f0430a6b5eecfb464548ddf48451779ae0a4
Handan <handan.hu@wisc.edu>
2026-03-25 17:16:14 -0700

    build: remove docs

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/smart-recipe/",
   build: {
-    outDir: "docs",
+    outDir: "dist",
   },
 });
